4ab66543c298be58a2fee5f25373b708765bf913
Johnny <wu-xuesong@qq.com>
2019-10-21 22:23:32 +0800

    remove log

--- src/crud.controller.ts
+++ src/crud.controller.ts
@@ -22,7 +22,6 @@ export class CrudController {
     description: 'Query options',
   })
   find(@CrudQuery('query') query: ICrudQuery = {}) {
-    console.log(this.model)
     let {
       where = get(this.crudOptions, 'routes.find.where', {}),
       limit = get(this.crudOptions, 'routes.find.limit', 10),
