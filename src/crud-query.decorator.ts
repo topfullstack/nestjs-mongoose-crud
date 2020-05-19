5437a036d937b1ae374da5e64c389a5f7bf99aa9
Nicolás <nmengualgomez@gmail.com>
2020-05-19 12:13:08 +0200

    Fixed #8 switchToHttp errror

--- src/crud-query.decorator.ts
+++ src/crud-query.decorator.ts
@@ -1,4 +1,4 @@
-import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+import { createParamDecorator } from "@nestjs/common";
 import { Request } from "express";
 
 export class ICrudQuery {
@@ -11,10 +11,9 @@ export class ICrudQuery {
   select?: string | any
 }
 
-export const CrudQuery = createParamDecorator((name = 'query', ctx: ExecutionContext) => {
-  const req: Request = ctx.switchToHttp().getRequest()
+export const CrudQuery = createParamDecorator((name = 'query', req: Request) => {
   try {
-    return JSON.parse(req.query[name])
+    return JSON.parse(req.query[name].toString())
   } catch (e) {
     return {}
   }
